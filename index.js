ef9c45728a73070b63889501dd17e6c928af0d6c
unknown <michael_jn_miranda@yahoo.com>
2015-05-03 13:26:47 -0500

    modified index.js do match heroku example to see if I can get this to work

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
 var express = require('express');
 var app = express();
-var encounter = 0;
-var path = require('path');
-var xmlparser = require('express-xml-bodyparser');
-var pizzaorders = [];
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.use(xmlparser());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World!'+ ++encounter)
-})*/
-app.get('/meow', function (req, res)
-{
-	res.send("mix")
-})
-
-app.get('/orders', function (req, res) {
-	var orderHtml = '';
-	for (var i = 0; i < pizzaorders.length; i++) {
-		var pizzaorder = pizzaorders[i];
-		orderHtml += pizzaorder;
-	}
-	res.send('<html><head><title>Orders</title></head><body><h1>Orders</h1>' + orderHtml + '</body></html>')
-})
-
-app.post('/order', function (req, res) {//makes the server listen for requests for the url /order
-  	
-  res.send('you sent me: ' + req.body.pizzaorder);
-  pizzaorders.push(req.body.pizzaorder)
-})
-
-app.use('/static', express.static(__dirname + '/static'));
-
-var server = app.listen(app.get('port'), function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
